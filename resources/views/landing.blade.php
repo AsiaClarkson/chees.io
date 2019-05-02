027cb4648e54d3544bd8d466bac212efcd677175
AsiaClarkson <aclarkso@usc.edu>
2019-05-01 19:36:02 -0700

    had a broken tag

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -15,7 +15,7 @@ noun: cheese; plural noun: cheeses<br>
 </div>
 <div class="jumbotron jumbotron-fluid bg-light">
   <div class="container">
-  <h4 class='text-center'>About Chees.io<h4>
+  <h4 class='text-center'>About Chees.io</h4>
   <p>Asia likes databases and cheeses. Chees.io is a labor of love made up of the two.</p>
   </div>
   </div>
