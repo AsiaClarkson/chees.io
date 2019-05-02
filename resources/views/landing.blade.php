9717b90ecf5b153255ad75ae1dbe0a8ea53e9182
AsiaClarkson <aclarkso@usc.edu>
2019-05-01 19:39:18 -0700

    added a class

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -14,7 +14,7 @@ noun: cheese; plural noun: cheeses<br>
 </div>
 </div>
 <div class="jumbotron jumbotron-fluid bg-light">
-  <div class="container">
+  <div class="container text-center">
   <h4 class='text-center'>About Chees.io</h4>
   <p>Asia likes databases and cheeses. Chees.io is a labor of love made up of the two.</p>
   </div>
